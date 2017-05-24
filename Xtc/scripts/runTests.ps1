71b66b2d3d0975c10fb7e677709a78958ca3643f
Simon Søndergaard <sisonder@microsoft.com>
2017-05-24 18:34:07 +0200

    dotnet test does no longer allow pasing xml option to xUnit and there is no support for test via .sln files

--- Xtc/scripts/runTests.ps1
+++ Xtc/scripts/runTests.ps1
@@ -12,10 +12,8 @@ function RunTests([string] $path)
         Push-Location
         Set-Location (Join-Path $rootDir $path)
         $projectName = [System.IO.Path]::GetFileName($path)
-        $outputXml = ".\bin\$configuration\netcoreapp1.0\$projectName.xunit-results.xml"
-        echo "Output XML: $outputXml"
 
-        dotnet test -c $configuration -xml "$outputXml"
+        dotnet test -c $configuration 
 
         if ($LASTEXITCODE -ne 0)
         {
